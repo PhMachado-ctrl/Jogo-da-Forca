b8d5863e9a3fb695f4f084f7d92ce3aa271e9cf1
PhMachado-ctrl <phmachado555@gmail.com>
2023-07-04 19:41:18 -0300

    var

---
+++ jogo-da-forca.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int Main(){
+    //Palavra e as letras que serão as alternativas que irão digitando, e a palavra que era se revelando.
+    char palavra[30], letra[1], secreta[30];
+    int tam, i, chances, acertos;
+
+    return 0;
+}
