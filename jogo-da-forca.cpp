884cfbf86fb765cd1b6b00a2f643e8d369e1a242
PhMachado-ctrl <phmachado555@gmail.com>
2023-07-04 20:11:42 -0300

    Atribuiçoes

--- jogo-da-forca.cpp
+++ jogo-da-forca.cpp
@@ -6,6 +6,13 @@ int Main(){
     //Palavra e as letras que serão as alternativas que irão digitando, e a palavra que era se revelando.
     char palavra[30], letra[1], secreta[30];
     int tam, i, chances, acertos;
+    bool acerto;
+
+    chances = 6;
+    tam = 0;
+    i = 0;
+    acerto = false;
+    acertos = 0;
 
     return 0;
 }
